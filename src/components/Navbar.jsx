5a2990d4178d1540fe851672cb9ad49f0d1a0092
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-23 22:07:05 +0200

    Install the daisyui plugin for the style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { logoImg } from "../utils";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <nav>
+        <img src={logoImg} alt="Logo" width={150} height={120} />
 
-export default Navbar
+        <div>
+          {["Heavy Duty", "Wooden Shelf", "Nice Shelf"].map((nav, i) => (
+            <div key={nav}>{nav}</div>
+          ))}
+        </div>
+
+        <div>
+            <img src="" alt="" />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
