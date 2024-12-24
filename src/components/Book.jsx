1e4454c53b47c8a27573c580b1d1ece283067e56
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 20:20:53 +0200

    Add the Book function

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -272,3 +272,38 @@ const Page = ({ number, front, back, page, opened, bookClosed, ...props }) => {
     </group>
   );
 };
+
+export const Book = ({ ...props }) => {
+  const [page] = useAtom(pageAtom);
+  const [delayedPage, setDelayedPage] = useState(page);
+
+  useEffect(() => {
+    let timeout;
+    const goToPage = () => {
+      setDelayedPage((delayedPage) => {
+        if (page === delayedPage) {
+          return delayedPage;
+        } else {
+          timeout = setTimeout(
+            () => {
+              goToPage();
+            },
+            Math.abs(page - delayedPage) > 2 ? 50 : 150
+          );
+          if (page > delayedPage) {
+            return delayedPage + 1;
+          }
+          if (page < delayedPage) {
+            return delayedPage - 1;
+          }
+        }
+      });
+    };
+    goToPage();
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [page]);
+
+  
+};
