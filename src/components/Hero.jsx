a7a9ca7ad47c544787e9db389d74618b6528788f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 02:50:17 +0200

    Add the gsap libraries inside the package.json file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='w-full nav-height relative'>
+        <div className='h-5/6 w-full flex-center flex-col'>
+        <p className='hero-title'>Heavy Duty Shelf</p>
+        </div>
+    </section>
   )
 }
 
