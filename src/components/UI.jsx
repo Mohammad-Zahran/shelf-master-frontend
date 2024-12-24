0ac21ae7d02a7300a2fac7b82c8bd9d33803f225
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 19:27:10 +0200

    Add the logic for navigating between pages of the Gallery Section

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -21,3 +21,21 @@ const pictures = [
 
 export const pageAtom = atom(0);
 
+export const pages = [
+  {
+    front: "book-cover",
+    back: pictures[0],
+  },
+];
+
+for (let i = 1; i < pictures.length - 1; i += 2) {
+  pages.push({
+    front: pictures[i % pictures.length],
+    back: pictures[(i + 1) % pictures.length],
+  });
+}
+
+pages.push({
+  front: pictures[pictures.length - 1],
+  back: "book-back",
+});
