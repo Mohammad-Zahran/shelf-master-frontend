18e7913308c3bbc5f964a3f407f3695f90e13846
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 03:08:55 +0200

    Add a handleVideoSecSet function to handle the responsivness of a video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,46 @@
-import React, { useState } from 'react'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import {heroVideo, smallHeroVideo} from '../utils';
-
+import React, { useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
-    const [videoSrc, setVideoSrc] = useState(window, innerWidth < 760 ? smallHeroVideo : heroVideo)
-    useGSAP(() => {
-        gsap.to('#hero', {opacity: 1, delay: 1.5})
-    }, [])
-  return (
-    <section className='w-full nav-height relative'>
-        <div className='h-5/6 w-full flex-center flex-col'>
-        <p id='hero' className='hero-title'>Heavy Duty Shelf</p>
-        <div className='md:w-10/12 w-9/12'>
-            <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
-               <source src={videoSrc} type='video/mp4' /> 
-            </video>
-        </div>
-        </div>
-    </section>
-  )
-}
+  const [videoSrc, setVideoSrc] = useState(
+    window,
+    innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
 
-export default Hero
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth <760) {
+        setVideoSrc(smallHeroVideo)
+    }
+    else {
+        setVideoSrc(heroVideo)
+    }
+  }
+
+  useGSAP(() => {
+    gsap.to("#hero", { opacity: 1, delay: 1.5 });
+  }, []);
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id="hero" className="hero-title">
+          Heavy Duty Shelf
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
