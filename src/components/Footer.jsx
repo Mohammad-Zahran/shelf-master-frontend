ae9091958bf52aa6f60ae58a1e7b278f690d1287
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 21:04:59 +0200

    Add the first column inside the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div>
+        {/* Column 1: Product Section */}
+        <div>
+          <h3>
+            <ul>
+              <li>Employee database</li>
+              <li>Payroll</li>
+              <li>Absences</li>
+              <li>Time tracking</li>
+              <li>Shift planner</li>
+              <li>Recruiting</li>
+            </ul>
+          </h3>
+        </div>
+        
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
