f1f071530588377d012e490d56bd7c08e301b1c0
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 19:42:27 +0200

    Add the notes to make the user understand the meaning of thise libraries

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -18,3 +18,14 @@ import {
 } from "three";
 import { degToRad } from "three/src/math/MathUtils.js";
 import { pageAtom, pages } from "./UI";
+
+/*
+Some Notes for what the imported classed are used for:
+useCursor and useTexture are utilities from @react-three/drei for 3D interactions.
+useFrame is a React hook provided by @react-three/fiber to hook into the render loop of the 3D scene.
+useAtom is a hook from Jotai, a state management library.
+Bone, Skeleton, SkinnedMesh: Used for skeletal animation.
+BoxGeometry, MeshStandardMaterial: Define 3D geometries and materials for rendering objects.
+Vector3, Color: Represent mathematical vectors and colors in 3D space.
+degToRad: A utility function for converting degrees to radians.
+*/
