034ee68c9c8e22b10d62352c00e96a65d8160c4b
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 21:06:51 +0200

    Add the second column inside the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,18 +6,26 @@ const Footer = () => {
       <div>
         {/* Column 1: Product Section */}
         <div>
-          <h3>
-            <ul>
-              <li>Employee database</li>
-              <li>Payroll</li>
-              <li>Absences</li>
-              <li>Time tracking</li>
-              <li>Shift planner</li>
-              <li>Recruiting</li>
-            </ul>
-          </h3>
+          <h3>Product</h3>
+          <ul>
+            <li>Employee database</li>
+            <li>Payroll</li>
+            <li>Absences</li>
+            <li>Time tracking</li>
+            <li>Shift planner</li>
+            <li>Recruiting</li>
+          </ul>
         </div>
-        
+
+        {/* Column 2: Information Section */}
+        <div>
+          <h3>Information</h3>
+          <ul>
+            <li>FAQ</li>
+            <li>Blog</li>
+            <li>Support</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
