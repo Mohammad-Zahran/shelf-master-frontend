a1cdeee11b1d701ae3e3ac1b45c69fd52823b4b7
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-24 02:54:01 +0200

    Test the gsap library inside the Hero Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.to('#hero', {opacity: 1})
+    }, [])
   return (
     <section className='w-full nav-height relative'>
         <div className='h-5/6 w-full flex-center flex-col'>
-        <p className='hero-title'>Heavy Duty Shelf</p>
+        <p id='hero' className='hero-title'>Heavy Duty Shelf</p>
         </div>
     </section>
   )
