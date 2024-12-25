7ca7117b6f7e096294d5132acd2bffc24560bf5e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-25 05:56:18 +0200

    Add the stats array of object

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Company = () => {
-  return (
-    <div>Company</div>
-  )
-}
+  const stats = [
+    {
+      id: 1,
+      icon: "/assets/images/exp1.svg",
+      number: 123,
+      label: "Shelves Inserted",
+    },
+    {
+      id: 2,
+      icon: "/assets/images/exp2.svg",
+      number: 84,
+      label: "Happy Clients",
+    },
+    {
+      id: 3,
+      icon: "/assets/images/exp3.svg",
+      number: 37,
+      label: "Awards Win",
+    },
+    {
+      id: 4,
+      icon: "/assets/images/exp4.svg",
+      number: 15,
+      label: "Years in Business",
+    },
+  ];
+  return <div>Company</div>;
+};
 
-export default Company
+export default Company;
