235bd0444e559dc12485547199191d88a1fc2fc1
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-25 22:45:42 +0200

    Add the useState hook inside the component

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import ModelView from "./ModelView";
+import { yellowImg } from "./../utils/index";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "Test",
+    img: yellowImg,
+  });
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
@@ -15,7 +22,7 @@ const Model = () => {
 
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            
+            <ModelView />
           </div>
         </div>
       </div>
