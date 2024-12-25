27a69e38fcb604a22db1c939bc3ca00db23d8471
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-25 23:45:02 +0200

    Add the Lights.jsx component

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,4 +1,4 @@
-import { View } from "@react-three/drei";
+import { PerspectiveCamera, View } from "@react-three/drei";
 import React from "react";
 
 const ModelView = ({
@@ -12,13 +12,17 @@ const ModelView = ({
 }) => {
   return (
     <View
-    index={index}
-    id={gsapType}
-    className="border-2 border-red-500 w-full h-full"
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full
+        ${index === 2} ? 'right-[-100%] : ''`}
     >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
 
+      <PerspectiveCamera makeDefault position={[0,0,4]} />
     </View>
-  )
+  );
 };
 
 export default ModelView;
