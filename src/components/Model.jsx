197c950006a574136ea77beeb79fd89aa5ec303f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-25 23:03:26 +0200

    Add the Canvas to render our 3d Models

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,6 +4,8 @@ import gsap from "gsap";
 import ModelView from "./ModelView";
 import { yellowImg } from "./../utils/index";
 import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import {View} from '@react-three/drei'
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -54,6 +56,20 @@ const Model = () => {
               item={model}
               size={size}
             />
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById('root')}
+            >
+              <View.Port />
+            </Canvas>
           </div>
         </div>
       </div>
