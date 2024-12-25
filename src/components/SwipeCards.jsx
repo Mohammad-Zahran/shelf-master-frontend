3bb4cc758e1084ecd2ff5bb4e5b14088842176f5
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-25 04:51:27 +0200

    Update the SwipeCards function

--- src/components/SwipeCards.jsx
+++ src/components/SwipeCards.jsx
@@ -4,7 +4,20 @@ import { motion, useMotionValue, useTransform } from "framer-motion";
 const SwipeCards = () => {
   const [cards, setCards] = useState(cardData);
 
-  return <div>SwipeCards</div>;
+  return (
+    <div
+      className="grid h-[500px] w-full place-items-center bg-neutral-100"
+      style={{
+        backgroundImage: `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='32' height='32' fill='none' stroke-width='2' stroke='%23d4d4d4'%3e%3cpath d='M0 .5H31.5V32'/%3e%3c/svg%3e")`,
+      }}
+    >
+      {cards.map((card) => {
+        return (
+          <Card key={card.id} cards={cards} setCards={setCards} {...card} />
+        );
+      })}
+    </div>
+  );
 };
 
 const Card = ({ id, url, setCards, cards }) => {
