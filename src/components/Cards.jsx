678dd7d8b44471169ad52c4dd9961c2fd7e82be3
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-26 20:33:53 +0200

    Add the Cards Like button inside the Cards components

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import {Link } from "react-router-dom";
+import {FaHeart} from "react-icons/fa";
 
 const Cards = ({ item }) => {
+    const [isHeartFillted, setIsHeartFillted] = useState(false)
+
+    const handleHeartClick = () => {
+        setIsHeartFillted(!isHeartFillted)
+    }
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl relative">
+        <div className={`rating gap-1 absolute right-2 top2 p-4 heartStar ${isHeartFillted ? "text-rose-500": "text-white"}`}
+        onClick={handleHeartClick}
+        >
+            <FaHeart className="h-5 w-5 cursor-pointer"/>
+        </div>
       <Link to={`/product/${item._id}`}>
         <figure>
           <img src={item.images}
