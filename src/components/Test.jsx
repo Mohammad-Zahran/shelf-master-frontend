c3fee139f770e3d178a6aa7143691c29c15864bb
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-26 19:35:02 +0200

    Create a dummy data for the products

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const Test = () => {
+  const [products, setProducts] = useState([]);
+  const slider = React.useRef(null);
+
+  useEffect(() => {
+    fetch("/")
+  }, [])
   const settings = {
     dots: true,
     infinite: false,
