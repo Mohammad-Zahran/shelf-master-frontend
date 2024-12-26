d3ac7c4d3f66bcfb11687cff3a798e397c6a4d79
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-26 03:03:24 +0200

    Clean the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Gallery from './components/Gallery';
-import Hero from './components/Hero';
-import Model from './components/Model';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import FAQ from './components/FAQ';
-import Reviews from './components/Reviews';
-import Company from './components/Company';
-import ModelViewer from './components/ModelViewer';
+import Gallery from "./components/Gallery";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import FAQ from "./components/FAQ";
+import Reviews from "./components/Reviews";
+import Company from "./components/Company";
+import ModelViewer from "./components/ModelViewer";
 
 const App = () => {
   return (
@@ -14,13 +13,12 @@ const App = () => {
       <Navbar />
       <Hero />
       <ModelViewer />
-      <Model />
       <Company />
       <Reviews />
       <FAQ />
       <Gallery />
       <Footer />
-      </>
+    </>
   );
 };
 
