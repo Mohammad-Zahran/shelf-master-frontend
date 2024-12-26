2c9e3c2e9f63b8379fe9b30e8ecec3b006aa02a7
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-26 20:15:25 +0200

    Update the main.jsx to have BrowserRouter inside it

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Cards = ({ item }) => {
   return (
-    <div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <Link to={`/product/${item._id}`}>
         <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes"
+          <img src={item.images}
+          className="hover:scale-105 transition-all duration-200 md:h-72"
           />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+      </Link>
+      <div className="card-body">
+        <h2 className="card-title">{item.name}</h2>
+        <p>{item.description}</p>
+        <div className="card-actions justify-between items-center mt-2">
+          <h5 className="font-semibold">
+            <span className="text-sm text-red">$</span>
+            {item.price}
+          </h5>
+          <button className="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
