1092bd9be18a05954d470253090eed488c207a5e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-28 21:03:20 +0200

    Add the Floor logic of the Floor inside the Room

--- src/components/floor-planner/Room.jsx
+++ src/components/floor-planner/Room.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Room = () => {
+const Room = ({ width, height }) => {
+  // Used to add the wall geometry of the planner.
+  const wallThickness = 0.1;
+  const wallHeight = 3;
   return (
-    <div>Room</div>
-  )
-}
+    <group>
+      {/* Floor */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.5, 0]}>
+        <planeGeometry args={[width, height]} />
+        <meshStandardMaterial color="lightgrey" />
+      </mesh>
+    </group>
+  );
+};
 
-export default Room
+export default Room;
