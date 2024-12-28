6fa52e6235447cc9df01afb450a321af0925e1d9
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-28 22:53:55 +0200

    Add the function handleKeyDown for moving the furniture using the keyboard

--- src/components/floor-planner/Furniture.jsx
+++ src/components/floor-planner/Furniture.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFloorPlanner } from "../../contexts/FloorPlannerContext";
 
@@ -10,7 +10,42 @@ const Furniture = ({ modelPath, scale = 1, index, roomWidth, roomHeight }) => {
     selectedFurnitureIndex,
     setSelectedFurnitureIndex,
   } = useFloorPlanner();
-  
+  const { position, rotation } = furnitureItems[index];
+
+  const isSelected = selectedFurnitureIndex;
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (!isSelected) return;
+
+      let [x, y, z] = position;
+      let [rotX, rotY, rotZ] = rotation;
+
+      switch (e.key) {
+        case "ArrowLeft":
+          if (x - 0.1 > -roomWidth / 2 + scale / 2) x -= 0.1;
+          break;
+        case "ArrowRight":
+          if (x + 0.1 < roomWidth / 2 - scale / 2) x += 0.1;
+          break;
+        case "ArrowUp":
+          if (z - 0.1 > -roomHeight / 2 + scale / 2) z -= 0.1;
+          break;
+        case "ArrowDown":
+          if (z + 0.1 < roomHeight / 2 - scale / 2) z += 0.1;
+          break;
+        case "r":
+          rotY += Math.PI / 16;
+          break;
+        case "l":
+          rotY -= Math.PI / 16;
+          break;
+        default:
+          return;
+      }
+    };
+  });
+
   return <div>Furniture</div>;
 };
 
