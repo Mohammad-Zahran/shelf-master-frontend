03b4cbe29835678c33f25b93eca4d7bf9dea1680
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-29 00:24:11 +0200

    Add all the html elements needed for the sidebar

--- src/components/floor-planner/Sidebar.jsx
+++ src/components/floor-planner/Sidebar.jsx
@@ -13,7 +13,25 @@ const Sidebar = (
   selectedFurnitureIndex,
   deleteFurniture
 ) => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <h2>Floor Planner</h2>
+
+      <button>Settings</button>
+
+      <button>Add Furniure</button>
+
+      <button>Delete Selected Furniture</button>
+
+      <button>Switch to {viewMode === "3D" ? "2D" : "3D"} View</button>
+
+      <div>
+        <label htmlFor="">Room Width: {width}m</label>
+        <input type="text" />
+        <label htmlFor="">Room Height: {height}m</label>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
