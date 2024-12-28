de2c6fa542794f6e77c57ab6cd4e952f7ab254cf
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-28 23:18:42 +0200

    Add a useEffect to fetch the models from the shelve.json in a daynamic way

--- src/components/floor-planner/FurniturePopup.jsx
+++ src/components/floor-planner/FurniturePopup.jsx
@@ -2,9 +2,31 @@ import React, { useEffect, useState } from "react";
 import { useFloorPlanner } from "../../contexts/FloorPlannerContext";
 
 const FurniturePopup = () => {
-  return (
-    <div>FurniturePopup</div>
-  )
-}
+  const { showPopup, setShowPopup, addFurniture } = useFloorPlanner();
+  const [furnitureModels, setFurnitureModels] = useState([]);
 
-export default FurniturePopup
+  // Fetch models dynamically from shelve.json
+  useEffect(() => {
+    const fetchModels = async () => {
+      try {
+        const response = await fetch("/shelve.json");
+        const data = await response.json();
+        setFurnitureModels(data);
+      } catch (error) {
+        console.error("Error loading furniture models:", error);
+      }
+    };
+
+    fetchModels();
+  }, []);
+
+  const handleAddFurniture = (modelPath) => {
+    addFurniture(modelPath); 
+    setShowPopup(false); 
+  };
+
+
+  return <div>FurniturePopup</div>;
+};
+
+export default FurniturePopup;
