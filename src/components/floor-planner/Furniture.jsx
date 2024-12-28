28638f092676551973c2ceca3a9f5be47f43b512
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-28 22:48:34 +0200

    Import floor planner inside the Furniture.jsx

--- src/components/floor-planner/Furniture.jsx
+++ src/components/floor-planner/Furniture.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFloorPlanner } from "../../contexts/FloorPlannerContext";
 
-const Furniture = () => {
-  return (
-    <div>Furniture</div>
-  )
-}
+const Furniture = ({ modelPath, scale = 1, index, roomWidth, roomHeight }) => {
+  const {
+    furnitureItems,
+    updateFurniturePosition,
+    updateFurnitureRotation,
+    selectedFurnitureIndex,
+    setSelectedFurnitureIndex,
+  } = useFloorPlanner();
+  
+  return <div>Furniture</div>;
+};
 
-export default Furniture
+export default Furniture;
