e40cbe76875ee07178ad2d6ae304da8338c4a9f3
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-28 21:07:08 +0200

    Add the Walls logic inside the Room component

--- src/components/floor-planner/Room.jsx
+++ src/components/floor-planner/Room.jsx
@@ -11,6 +11,33 @@ const Room = ({ width, height }) => {
         <planeGeometry args={[width, height]} />
         <meshStandardMaterial color="lightgrey" />
       </mesh>
+
+      {/* Walls */}
+      <mesh position={[0, wallHeight / 2, -height / 2]}>
+        <boxGeometry args={[width, wallHeight, wallThickness]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+      <mesh
+        position={[width / 2, wallHeight / 2, 0]}
+        rotation={[0, -Math.PI / 2, 0]}
+      >
+        <boxGeometry args={[height, wallHeight, wallThickness]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+      <mesh
+        position={[-width / 2, wallHeight / 2, 0]}
+        rotation={[0, Math.PI / 2, 0]}
+      >
+        <boxGeometry args={[height, wallHeight, wallThickness]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+      <mesh
+        position={[0, wallHeight / 2, height / 2]}
+        rotation={[0, Math.PI, 0]}
+      >
+        <boxGeometry args={[width, wallHeight, wallThickness]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
     </group>
   );
 };
