ff2bc338f88da700a070b5eb195ee09c515be146
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-29 21:29:01 +0200

    Add the Modal code for a popup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         <a>Add Review</a>
       </li>
       <li>
-        <a  href="/floor-planner">3d Floor Planner</a>
+        <a href="/floor-planner">3d Floor Planner</a>
       </li>
       <li>
         <a>Shelf Assistant AI</a>
@@ -101,9 +101,29 @@ const Navbar = () => {
               </div>
             </div>
 
-            <button className="btn">
+            <button
+              onClick={() => document.getElementById("my_modal_5").showModal()}
+              className="btn"
+            >
               <FaRegUser /> Login
             </button>
+            <dialog
+              id="my_modal_5"
+              className="modal modal-bottom sm:modal-middle"
+            >
+              <div className="modal-box">
+                <h3 className="font-bold text-lg">Hello!</h3>
+                <p className="py-4">
+                  Press ESC key or click the button below to close
+                </p>
+                <div className="modal-action">
+                  <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn">Close</button>
+                  </form>
+                </div>
+              </div>
+            </dialog>
           </div>
         </div>
       </nav>
