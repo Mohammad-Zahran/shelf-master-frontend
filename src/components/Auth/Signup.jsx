9242c07bac832c8db477bca4729bc87077e9a531
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 01:37:25 +0200

    Add the state variable for registering and submitting the data

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -6,71 +6,81 @@ import { FaGithub } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div
-      className="modal-action flex flex-col justify-center mt-0"
-      method="dialog"
-    >
-      <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-        <h3 className="font-bold text-lg">Please Login</h3>
+    <div>
+      <div
+        className="modal-action flex flex-col justify-center mt-0"
+        method="dialog"
+      >
+        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+          <h3 className="font-bold text-lg">Please Login</h3>
 
-        {/* email */}
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="email"
-            placeholder="email"
-            className="input input-bordered"
-            {...register("email")}
-          />
+          {/* email */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              {...register("email")}
+            />
+          </div>
+
+          {/* password */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              {...register("password")}
+            />
+            <label className="label mt-1">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+
+          {/* error */}
+
+          {/* login btn */}
+          <div className="form-control mt-6">
+            <input type="submit" value="Login" className="btn normal" />
+          </div>
+
+          <p className="text-center my-2">
+            Don't have an account?{" "}
+            <Link to="/signup" className="underline text-steelBlue ml-1">
+              Signup Now
+            </Link>
+          </p>
+        </form>
+
+        {/* Social sign in */}
+        <div className="text-center space-3 mb-5">
+          <button className="btn btn-circle normal">
+            <FaGoogle />
+          </button>
+          <button className="btn btn-circle normal">
+            <FaFacebookF />
+          </button>
+          <button className="btn btn-circle normal">
+            <FaGithub />
+          </button>
         </div>
-
-        {/* password */}
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            placeholder="password"
-            className="input input-bordered"
-            {...register("password")}
-          />
-          <label className="label mt-1">
-            <a href="#" className="label-text-alt link link-hover">
-              Forgot password?
-            </a>
-          </label>
-        </div>
-
-        {/* error */}
-
-        {/* login btn */}
-        <div className="form-control mt-6">
-          <input type="submit" value="Login" className="btn normal" />
-        </div>
-
-        <p className="text-center my-2">
-          Don't have an account?{" "}
-          <Link to="/signup" className="underline text-steelBlue ml-1">
-            Signup Now
-          </Link>
-        </p>
-      </form>
-
-      {/* Social sign in */}
-      <div className="text-center space-3 mb-5">
-        <button className="btn btn-circle normal">
-          <FaGoogle />
-        </button>
-        <button className="btn btn-circle normal">
-          <FaFacebookF />
-        </button>
-        <button className="btn btn-circle normal">
-          <FaGithub />
-        </button>
       </div>
     </div>
   );
