c8782e8fefa5f19ccf8bdce5ff16182153b001dd
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 21:27:57 +0200

    Create the asynchrnous function to call the data of the products

--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-    const [product, setProduct] = useState([]);
-    const[filteredItem, setFilteredItem] = useState([]);
-    const [selectedCategory, setSelectedCategory] = useState("all");
-    const [sortOption, setSortOption] = useState("default");
+  const [product, setProduct] = useState([]);
+  const [filteredItem, setFilteredItem] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
 
-    // loading data
-    
+  // loading data
+  useEffect(() => {
+    // fetch data from the backend
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/product.json");
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log("Error fetching the products", error);
+      }
+    };
+    // call the function
+    fetchData();
+  }, []);
 
   return (
     <div>
-        {/* Product shop section */}
-        <div className='section-container'>
-
-        </div>
+      {/* Product shop section */}
+      <div className="section-container"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
