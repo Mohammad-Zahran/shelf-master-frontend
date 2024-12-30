a946eaefff07db0de4f5cd79f7d0812d87f1628e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 07:04:00 +0200

    Add a condition for viewing the profile picture

--- src/components/Auth/Profile.jsx
+++ src/components/Auth/Profile.jsx
@@ -12,10 +12,14 @@ const Profile = ({ user }) => {
             className="drawer-button btn btn-ghost btn-circle avatar"
           >
             <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+              {user.photoURL ? (
+                <img alt="Tailwind CSS Navbar component" src={user.photoURL} />
+              ) : (
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              )}
             </div>
           </label>
         </div>
