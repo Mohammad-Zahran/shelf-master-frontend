527827ca7b5eaa63ab52551dc648b5cbe060d6d6
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 18:03:25 +0200

    Add the inputs inside a div and change the style

--- src/pages/dashboard/UpdateProfile.jsx
+++ src/pages/dashboard/UpdateProfile.jsx
@@ -2,39 +2,42 @@ import React from "react";
 
 const UpdateProfile = () => {
   return (
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="email"
-            placeholder="email"
-            className="input input-bordered"
-            required
-          />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            placeholder="password"
-            className="input input-bordered"
-            required
-          />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">
-              Forgot password?
-            </a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
+    <div className="flex items-center justify-center h-screen">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+        <form className="card-body">
+          <h3 className="font-bold">Update Your Profile</h3>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="your name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Upload Photo</span>
+            </label>
+            <input
+              type="text"
+              placeholder="photoURL"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn normal">Update</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
