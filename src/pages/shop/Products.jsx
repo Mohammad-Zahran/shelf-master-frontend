2be7824d7dc89634730cf6427b0be0a6e5952af7
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-31 01:32:30 +0200

    Fix the selector style

--- src/pages/shop/Products.jsx
+++ src/pages/shop/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Cards from "../../components/home/Cards";
+import { LuSettings2 } from "react-icons/lu";
 
 const Products = () => {
   const [product, setProduct] = useState([]);
@@ -75,18 +76,84 @@ const Products = () => {
       {/* Product shop section */}
       <div className="section-container">
         {/* filtering and sorting */}
-        <div className="mt-24">
+        <div className="mt-24 flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8">
           {/* All Category btns */}
           <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex=wrap">
-            <button onClick={showAll} className={selectedCategory === "all" ? "active" : ""}>All</button>
-            <button onClick={() => filterItems("Heavy-Duty")} className={selectedCategory === "Heavy-Duty" ? "active" : ""}>Heavy-Duty</button>
-            <button onClick={() => filterItems("Adjustable")} className={selectedCategory === "Adjustable" ? "active" : ""}>Adjustable</button>
-            <button onClick={() => filterItems("Modern")} className={selectedCategory === "Modern" ? "active" : ""}>Modern</button>
-            <button onClick={() => filterItems("Rustic")} className={selectedCategory === "Rustic" ? "active" : ""}>Rustic</button>
-            <button onClick={() => filterItems("Industrial")} className={selectedCategory === "Industrial" ? "active" : ""}>Industrial</button>
-            <button onClick={() => filterItems("Decorative")} className={selectedCategory === "Decorative" ? "active" : ""}>Decorative</button>
-            <button onClick={() => filterItems("Kids")} className={selectedCategory === "Kids" ? "active" : ""}>Kids</button>
-            <button onClick={() => filterItems("Luxury")} className={selectedCategory === "Luxury" ? "active" : ""}>Luxury</button>
+            <button
+              onClick={showAll}
+              className={selectedCategory === "all" ? "active" : ""}
+            >
+              All
+            </button>
+            <button
+              onClick={() => filterItems("Heavy-Duty")}
+              className={selectedCategory === "Heavy-Duty" ? "active" : ""}
+            >
+              Heavy-Duty
+            </button>
+            <button
+              onClick={() => filterItems("Adjustable")}
+              className={selectedCategory === "Adjustable" ? "active" : ""}
+            >
+              Adjustable
+            </button>
+            <button
+              onClick={() => filterItems("Modern")}
+              className={selectedCategory === "Modern" ? "active" : ""}
+            >
+              Modern
+            </button>
+            <button
+              onClick={() => filterItems("Rustic")}
+              className={selectedCategory === "Rustic" ? "active" : ""}
+            >
+              Rustic
+            </button>
+            <button
+              onClick={() => filterItems("Industrial")}
+              className={selectedCategory === "Industrial" ? "active" : ""}
+            >
+              Industrial
+            </button>
+            <button
+              onClick={() => filterItems("Decorative")}
+              className={selectedCategory === "Decorative" ? "active" : ""}
+            >
+              Decorative
+            </button>
+            <button
+              onClick={() => filterItems("Kids")}
+              className={selectedCategory === "Kids" ? "active" : ""}
+            >
+              Kids
+            </button>
+            <button
+              onClick={() => filterItems("Luxury")}
+              className={selectedCategory === "Luxury" ? "active" : ""}
+            >
+              Luxury
+            </button>
+          </div>
+
+          {/* sorting base Filtering */}
+          <div className="flex justify-end items-center mb-2 rounded-sm">
+            {/* Sorting Dropdown with Icon */}
+            <div className="relative">
+              <LuSettings2 className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500 h-4 w-4 pointer-events-none" />
+              <select
+                name="sort"
+                id="sort"
+                onClick={(e) => handleSortChange(e.target.value)}
+                value={sortOption}
+                className="pl-8 pr-2 py-1 rounded-sm border border-gray-300 focus:outline-none focus:ring focus:ring-blue-200"
+              >
+                <option value="default">Default</option>
+                <option value="A-Z">A-Z</option>
+                <option value="Z-A">Z-A</option>
+                <option value="low-to-high">Low to High</option>
+                <option value="high-to-low">High to Low</option>
+              </select>
+            </div>
           </div>
         </div>
 
