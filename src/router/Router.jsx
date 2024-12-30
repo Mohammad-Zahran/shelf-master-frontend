d743d159e272bb8ed59fc6d60a410cef01f421f7
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 17:50:13 +0200

    Add the update-profile route inside the Router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/home/Home";
 import FloorPlanner from "../pages/floor-planner/FloorPlanner";
 import { FloorPlannerProvider } from "../contexts/FloorPlannerContext";
 import Signup from "../components/Auth/Signup";
+import UpdateProfile from "../pages/dashboard/UpdateProfile";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+      {
+        path: "/update-profile",
+        element: <UpdateProfile />
+      }
     ],
   },
   {
