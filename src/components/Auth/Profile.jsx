97c0b6dbd8a71b449bd82c25c2a4ae6bd235683e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 06:57:40 +0200

    Add an avatar profile picture from daisyUI

--- src/components/Auth/Profile.jsx
+++ src/components/Auth/Profile.jsx
@@ -3,15 +3,20 @@ import React from "react";
 const Profile = ({ user }) => {
   return (
     <div>
-      <div className="drawer drawer-end">
+      <div className="drawer drawer-end z-50">
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           {/* Page content here */}
           <label
             htmlFor="my-drawer-4"
-            className="drawer-button btn btn-primary"
+            className="drawer-button btn btn-ghost btn-circle avatar"
           >
-            Open drawer
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
           </label>
         </div>
         <div className="drawer-side">
@@ -23,10 +28,16 @@ const Profile = ({ user }) => {
           <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
             {/* Sidebar content here */}
             <li>
-              <a>Sidebar Item 1</a>
+              <a>Profile</a>
             </li>
             <li>
-              <a>Sidebar Item 2</a>
+              <a>Order</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
             </li>
           </ul>
         </div>
