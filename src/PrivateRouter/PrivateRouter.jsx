e409ce79d6e88a38a48015365c30b4649a3aaf7c
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 17:07:58 +0200

    Add the Navigation to signup if not logged in

--- src/PrivateRouter/PrivateRouter.jsx
+++ src/PrivateRouter/PrivateRouter.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import LoadingSpinner from "../components/LoadingSpinner";
 
-const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
-}
+const PrivateRouter = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
 
-export default PrivateRouter
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/signup" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRouter;
