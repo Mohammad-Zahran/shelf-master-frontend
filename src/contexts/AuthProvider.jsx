ff71b31666964d7c13d590fd5398c68f5b68b80d
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 04:52:52 +0200

    Create a function to create a new account

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,18 +1,36 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
+const AuthProvider = ({ children }) => {
+  const auth = getAuth(app);
 
-    const authInfo = {
-        user
-    }
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // create an account
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
+  const authInfo = {
+    user,
+  };
   return (
-    <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
