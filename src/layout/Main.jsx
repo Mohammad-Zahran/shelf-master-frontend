0d34b40b1ddcf401269d4d6f817b6598eaad0b1e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 16:54:34 +0200

    Update the Main.jsx

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,32 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import LoadingSpinner from "../components/LoadingSpinner";
+import { AuthContext } from "../contexts/AuthProvider";
 
 const Main = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading for 2 seconds (replace this logic with your actual loading mechanism)
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer); // Cleanup timer on component unmount
-  }, []);
-
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
+  const { loading } = useContext(AuthContext);
 
   return (
-    <div>
-      <Navbar />
-      <div>
-        <Outlet />
-      </div>
-      <Footer />
+    <div className="bg-prigmayBG">
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div>
+          <Navbar />
+          <div className="min-h-screen">
+            <Outlet />
+          </div>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
