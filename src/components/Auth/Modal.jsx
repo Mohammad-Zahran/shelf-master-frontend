5ef130fe1a8a4ceb46efdf67e331727a10c8dd96
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-30 03:39:52 +0200

    Add a button to navigate the user outside the form

--- src/components/Auth/Modal.jsx
+++ src/components/Auth/Modal.jsx
@@ -68,6 +68,13 @@ const Modal = () => {
                 Signup Now
               </Link>
             </p>
+
+            <button
+            htmlFor="my_modal_5"
+            onClick={() => document.getElementById("my_modal_5").close()}
+            className="btn normal btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
           </form>
 
           {/* Social sign in */}
