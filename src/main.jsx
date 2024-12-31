d37b7e9f0bf1688f72928f989bf2ca35e9298a1a
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-31 20:53:02 +0200

    Import the tanstack library inside the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,18 @@ import "./index.css";
 import router from "./router/Router.jsx";
 import AuthProvider from "./contexts/AuthProvider.jsx";
 
+// tankstack
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>,
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </AuthProvider>
 );
