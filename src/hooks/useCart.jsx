7ce76679ae591a7743b2cca42a8c04d4eb12a88f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-31 21:02:25 +0200

    Add the logic needed tp use the useCart for fetching data

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
 
 const useCart = () => {
-  return (
-    <div>useCart</div>
-  )
-}
+  const { user } = useContext(AuthContext);
+  const { refetch, data: cart = [] } = useQuery({
+    queryKey: ["carts", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:8080/carts?email=${user?.email}`
+      );
+      return res.json();
+    },
+  });
 
-export default useCart
+  return [cart, refetch];
+};
+
+export default useCart;
