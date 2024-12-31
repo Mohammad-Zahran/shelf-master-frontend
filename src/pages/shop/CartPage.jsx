b3f6bc32b3a4c948208efe33f4047346bc241b81
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-12-31 22:30:01 +0200

    Add a sweatalert to handle the delete of a cart item

--- src/pages/shop/CartPage.jsx
+++ src/pages/shop/CartPage.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import useCart from "./../../hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const CartPage = () => {
   const [cart, refetch] = useCart();
+
+  // handledelete btn
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
   return (
     <div className="section-container ">
       <div>
@@ -28,15 +51,12 @@ const CartPage = () => {
                     <div className="flex items-center gap-3">
                       <div className="avatar">
                         <div className="mask mask-squircle h-12 w-12">
-                          <img
-                            src={item.images[0]}
-                            alt=""
-                          />
+                          <img src={item.images[0]} alt="" />
                         </div>
                       </div>
                     </div>
                   </td>
-                  <td>
+                  <td className="font-medium">
                     {item.name}
                     <br />
                     <span className="badge badge-ghost badge-sm">
@@ -46,7 +66,12 @@ const CartPage = () => {
                   <td>{item.quantity}</td>
                   <td>{item.price}</td>
                   <th>
-                    <button className="btn btn-ghost btn-xs">details</button>
+                    <button
+                      className="btn btn-ghost text-red-600 btn-xs"
+                      onClick={() => handleDelete(item)}
+                    >
+                      <FaTrash />
+                    </button>
                   </th>
                 </tr>
               ))}
