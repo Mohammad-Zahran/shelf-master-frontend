1252c8cf010c08fefbe3a82b1e9417d52b5052e4
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-01 04:12:39 +0200

    Add the useWishList Logic insie the component

--- src/hooks/useWishList.jsx
+++ src/hooks/useWishList.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
 
 const useWishList = () => {
-  return (
-    <div>useWishList</div>
-  )
-}
+  const { user } = useContext(AuthContext);
+  const { refetch, data: wishListData = [] } = useQuery({
+    queryKey: ["wishlist", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:8080/wishlists?email=${user?.email}`
+      );
+      if (!res.ok) {
+        throw new Error("Failed to fetch wishlist data");
+      }
+      return res.json();
+    },
+    enabled: !!user?.email, // Ensure query runs only when user is logged in
+  });
 
-export default useWishList
+  return [wishListData, refetch];
+};
+
+export default useWishList;
