f92b5c3516e85418fcf049b8057e5da26dcc238a
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-05 20:32:25 +0200

    Test the API KEY of the stripe payment processer

--- src/pages/shop/Payment.jsx
+++ src/pages/shop/Payment.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_Stripe_PK);
+console.log(stripePromise);
 
 const Payment = () => {
   return (
-    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 py-28'>
-        
+    <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 py-28">
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
-  )
-}
+  );
+};
 
-export default Payment
+export default Payment;
