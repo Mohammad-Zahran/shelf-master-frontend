962c11e12e22036d61ec3f2d476ad6bdf93a94fd
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-05 20:25:44 +0200

    Create a checkoutForm.jsx Page

--- src/pages/shop/Payment.jsx
+++ src/pages/shop/Payment.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Payment = () => {
   return (
-    <div>Payment</div>
+    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 py-28'>
+        
+    </div>
   )
 }
 
