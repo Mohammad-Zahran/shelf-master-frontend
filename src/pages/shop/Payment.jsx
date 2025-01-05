bec932eaaf3932566c78284e653fea2d991f10f1
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-05 20:36:59 +0200

    Import the useCart Hook inside the Payment.jsx

--- src/pages/shop/Payment.jsx
+++ src/pages/shop/Payment.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import CheckoutForm from "./CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import useCart from "../../hooks/useCart";
 
 const stripePromise = loadStripe(import.meta.env.VITE_Stripe_PK);
-console.log(stripePromise);
-
 const Payment = () => {
+    const [cart] = useCart();
+    console.log(cart);
+    // calculate the checkout prices
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 py-28">
       <Elements stripe={stripePromise}>
-        <CheckoutForm />
+        <CheckoutForm cart={cart} />
       </Elements>
     </div>
   );
