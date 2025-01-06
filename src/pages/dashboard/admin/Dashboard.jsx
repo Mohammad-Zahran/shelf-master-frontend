6128c3587ccade9615d4e736ef0f84cec40bee0d
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-07 01:13:10 +0200

    Add a stats div from daisyui plugin

--- src/pages/dashboard/admin/Dashboard.jsx
+++ src/pages/dashboard/admin/Dashboard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const Dashboard = () => {
+  const { user } = useAuth();
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="w-full md:w-[1250px] px-4 mx-auto">
+      <h2 className="text-2xl font-semibold my-4">
+        Hi, <span className="text-steelBlue">{user.displayName}</span>
+      </h2>
 
-export default Dashboard
+      {/* Stats div */}
+      <div className="stats stats-vertical lg:stats-horizontal shadow">
+        <div className="stat">
+          <div className="stat-title">Downloads</div>
+          <div className="stat-value">31K</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">New Users</div>
+          <div className="stat-value">4,200</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">New Registers</div>
+          <div className="stat-value">1,200</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
