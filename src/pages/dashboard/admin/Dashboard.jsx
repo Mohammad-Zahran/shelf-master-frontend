f2cc82cdb45f0d4a29c89f9193977d8138e6a53e
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-07 01:53:13 +0200

    Fetch the adminStats API from the Dashboard

--- src/pages/dashboard/admin/Dashboard.jsx
+++ src/pages/dashboard/admin/Dashboard.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const Dashboard = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { refetch, data: stats = [] } = useQuery({
+    queryKey: ["stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/adminStats");
+      return res.data;
+    },
+  });
+
+  console.log(stats);
+
+  
   return (
     <div className="w-full md:w-[1250px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4">
@@ -21,6 +35,12 @@ const Dashboard = () => {
           <div className="stat-title">New Users</div>
           <div className="stat-value">4,200</div>
           <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">New Registers</div>
+          <div className="stat-value">1,200</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
 
         <div className="stat">
