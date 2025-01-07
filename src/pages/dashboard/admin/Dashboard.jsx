b203e884054625f975598e959843812006adcacb
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-07 03:56:33 +0200

    Test to see if the order-stats data is working

--- src/pages/dashboard/admin/Dashboard.jsx
+++ src/pages/dashboard/admin/Dashboard.jsx
@@ -14,9 +14,17 @@ const Dashboard = () => {
     },
   });
 
-  console.log(stats);
+  // console.log(state);
+  const { data: chartData = [] } = useQuery({
+    queryKey: ["order-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/orderStats");
+      return res.data;
+    },
+  });
 
-  
+  console.log(chartData);
+
   return (
     <div className="w-full md:w-[1250px] px-4 mx-auto">
       <h2 className="text-2xl font-semibold my-4">
@@ -24,31 +32,34 @@ const Dashboard = () => {
       </h2>
 
       {/* Stats div */}
-      <div className="stats stats-vertical lg:stats-horizontal shadow">
+      <div className="stats stats-vertical w-full lg:stats-horizontal shadow">
         <div className="stat">
-          <div className="stat-title">Downloads</div>
-          <div className="stat-value">31K</div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">{stats.revenue}</div>
           <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
 
         <div className="stat">
-          <div className="stat-title">New Users</div>
-          <div className="stat-value">4,200</div>
+          <div className="stat-title">Shelf Items</div>
+          <div className="stat-value">{stats.shelfItems}</div>
           <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
         <div className="stat">
-          <div className="stat-title">New Registers</div>
-          <div className="stat-value">1,200</div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
 
         <div className="stat">
-          <div className="stat-title">New Registers</div>
-          <div className="stat-value">1,200</div>
+          <div className="stat-title">All Orders</div>
+          <div className="stat-value">{stats.orders}</div>
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
       </div>
+
+      {/* charts and graphs */}
+
     </div>
   );
 };
