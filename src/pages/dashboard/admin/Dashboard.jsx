c1724143ac11e81d68e7a825958212b817e7ce35
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-07 04:59:48 +0200

    Add a barchart from the rechart library

--- src/pages/dashboard/admin/Dashboard.jsx
+++ src/pages/dashboard/admin/Dashboard.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Dashboard = () => {
   const { user } = useAuth();
@@ -59,7 +68,37 @@ const Dashboard = () => {
       </div>
 
       {/* charts and graphs */}
+      <div className="mt-12">
+        {/* bar chart */}
+        <div>
+          <div style={{ width: "100%", height: 300 }}>
+            <ResponsiveContainer>
+              <AreaChart
+                data={chartData}
+                margin={{
+                  top: 10,
+                  right: 30,
+                  left: 0,
+                  bottom: 0,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="category" />
+                <YAxis />
+                <Tooltip />
+                <Area
+                  type="monotone"
+                  dataKey="revenue"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+      </div>
 
+      {/* charts and graphs */}
     </div>
   );
 };
