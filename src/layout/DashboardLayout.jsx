b9cc0c565c1c517ec11c91be7ee099fb728e0f63
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-09 10:48:11 +0200

    Update the responsive Navbar inside the dashboard Layout

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -49,7 +49,7 @@ const DashboardLayout = () => {
         </Link>
       </li>
 
-      {/* Menu Item: Accounts */}
+      {/* Add Shelf */}
       <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
         <Link
           to="/dashboard/add-product"
@@ -60,7 +60,7 @@ const DashboardLayout = () => {
         </Link>
       </li>
 
-      {/* Menu Item: Accounts */}
+      {/* Manage Items */}
       <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
         <Link
           to="/dashboard/manage-items"
@@ -71,7 +71,7 @@ const DashboardLayout = () => {
         </Link>
       </li>
 
-      {/* Menu Item: Accounts */}
+      {/* Manage Orders */}
       <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
         <Link
           to="/dashboard/manage-orders"
@@ -123,7 +123,7 @@ const DashboardLayout = () => {
                     </button>
                   </div>
                   <ul className="menu text-base-content mt-4">
-                    {/* Menu Item: Dashboard */}
+                    {/*Dashboard */}
                     <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
                       <Link
                         to="/dashboard"
@@ -135,7 +135,7 @@ const DashboardLayout = () => {
                       </Link>
                     </li>
 
-                    {/* Menu Item: Accounts */}
+                    {/*Accounts */}
                     <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
                       <Link
                         to="/dashboard/users"
@@ -147,24 +147,41 @@ const DashboardLayout = () => {
                       </Link>
                     </li>
 
-                    {/* Additional menu items */}
-                    {Array(4)
-                      .fill(null)
-                      .map((_, i) => (
-                        <li
-                          key={i}
-                          className="text-lg text-[#273240] hover:text-[#5A6ACF]"
-                        >
-                          <Link
-                            to="/dashboard/users"
-                            className="flex items-center space-x-2"
-                            onClick={() => setIsMenuOpen(false)}
-                          >
-                            <FaUser className="text-steelBlue" />
-                            <span>Accounts</span>
-                          </Link>
-                        </li>
-                      ))}
+                    {/* Add Shelf */}
+                    <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
+                      <Link
+                        to="/dashboard/add-product"
+                        className="flex items-center space-x-2"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <IoMdAddCircle className="text-steelBlue" />
+                        <span>Add Shelf</span>
+                      </Link>
+                    </li>
+
+                    {/* Manage Items */}
+                    <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
+                      <Link
+                        to="/dashboard/manage-items"
+                        className="flex items-center space-x-2"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <FaRegEdit className="text-steelBlue" />
+                        <span>Manage Shelfs</span>
+                      </Link>
+                    </li>
+
+                    {/* Manage Orders */}
+                    <li className="text-lg text-[#273240] hover:text-[#5A6ACF]">
+                      <Link
+                        to="/dashboard/manage-orders"
+                        className="flex items-center space-x-2"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <FaRegEdit className="text-steelBlue" />
+                        <span>Manage Orders</span>
+                      </Link>
+                    </li>
                   </ul>
                 </div>
               )}
