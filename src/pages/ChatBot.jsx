3092f4119bb2942832e7b5b22e21e8b38a1278a8
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-10 08:51:19 +0200

    Add gradiant to bg color of the page

--- src/pages/ChatBot.jsx
+++ src/pages/ChatBot.jsx
@@ -3,8 +3,19 @@ import React from "react";
 const ChatBot = () => {
   return (
     <div className="h-screen py-6 relative sm:px-28 overflow-hidden flex-col justify-between align-middle">
-      <div>header</div>
+      {/* gradients */}
+      <div className="gradient-01 z-0 absolute"></div>
+      <div className="gradient-02 z-0 absolute"></div>
+
+      {/* header */}
+      <div className="font-bold text-2xl text-center mb-3">
+        Start Talking with Shelfie
+      </div>
+
+      {/* body */}
       <div>body</div>
+
+      {/* input */}
       <div>body</div>
     </div>
   );
