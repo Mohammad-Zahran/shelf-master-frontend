51ef009a6bebff759468c084dce9b388c2ecd800
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-10 09:14:18 +0200

    Create the style of ai message

--- src/components/ChatBody.jsx
+++ src/components/ChatBody.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const ChatBody = () => {
+
+    const aiStyle = "bg-steelBlue bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto "
   return (
     <div className="flex flex-col gap-4">
 
@@ -12,7 +14,8 @@ const ChatBody = () => {
       </div>
 
       {/* ai message */}
-      <div>
+      <div className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%]">
+        <pre className="whitespaces-pre-wrap ${aiStyle}`}>
         <pre>
             <span>Yeah, I can help you with anything</span>
         </pre>
