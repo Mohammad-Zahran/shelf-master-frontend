d9291e05be34f04b62af0476eb11e7dbe3cad28f
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2025-01-21 03:17:51 +0200

    Make the port private

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: `http://localhost:${import.meta.env.VITE_SERVER_PORT}`,
 });
 
 const useAxiosPublic = () => {
